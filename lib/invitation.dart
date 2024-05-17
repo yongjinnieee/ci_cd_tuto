2f1ac902e29ddd6566436a44258034a93079ac7d
yongjinnieee <yongjintan9910@gmail.com>
2024-05-17 10:20:52 +0800

    update

--- lib/invitation.dart
+++ lib/invitation.dart
@@ -48,107 +48,109 @@ class _InvitationState extends State<Invitation> {
         backgroundColor: Colors.white,
       ),
       body: Center(
-        child: Column(
-          children: [
-            Text('Invitation',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-            Text(
-              'Invite users',
-              style: TextStyle(fontSize: 20),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text('Owner\'s Name', style: TextStyle(fontSize: 23)),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Invitation',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              Text(
+                'Invite users',
+                style: TextStyle(fontSize: 20),
               ),
-            ),
-            SizedBox(height: 10.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(),
-                    hintText: 'Type here',
-                    hintStyle: TextStyle(
-                      fontSize: 20.0,
-                    )),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    'Owner\'s Phone Number',
-                    style: TextStyle(
-                      fontSize: 23,
-                    ),
-                  ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Owner\'s Name', style: TextStyle(fontSize: 23)),
+                  ],
                 ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(11.0),
-              child: Row(
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      hintText: 'Type here',
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                      )),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  CountryFlag.fromCountryCode(
-                    'MY',
-                    height: 48,
-                    width: 62,
-                    borderRadius: 8,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '+60',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                          hintText: 'Enter your phone number',
-                          hintStyle: TextStyle(
-                            fontSize: 20.0,
-                          )),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Owner\'s Phone Number',
+                      style: TextStyle(
+                        fontSize: 23,
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text(
-                'Submit',
-                style: TextStyle(fontSize: 23),
+              Padding(
+                padding: const EdgeInsets.all(11.0),
+                child: Row(
+                  children: [
+                    CountryFlag.fromCountryCode(
+                      'MY',
+                      height: 48,
+                      width: 62,
+                      borderRadius: 8,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+60',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            hintText: 'Enter your phone number',
+                            hintStyle: TextStyle(
+                              fontSize: 20.0,
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                  fixedSize: Size(370, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  backgroundColor: Color.fromRGBO(155, 154, 157, 1)),
-            ),
-          ],
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Submit',
+                  style: TextStyle(fontSize: 23),
+                ),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(370, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    backgroundColor: Color.fromRGBO(155, 154, 157, 1)),
+              ),
+            ],
+          ),
         ),
       ),
     );
